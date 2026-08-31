c9823ad735009274f175cca8459cfa8e487636c5
fufesou <linlong1266@gmail.com>
2026-08-31 09:59:56 +0800

    refact(logs): keep logs, 31 -> 90 days
    Signed-off-by: fufesou <linlong1266@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -459,7 +459,7 @@ pub fn init_log(_is_async: bool, _name: &str) -> Option<flexi_logger::LoggerHand
                     .rotate(
                         Criterion::Age(Age::Day),
                         Naming::Timestamps,
-                        Cleanup::KeepLogFiles(31),
+                        Cleanup::KeepLogFiles(90),
                     )
                     .start()
                     .ok();
